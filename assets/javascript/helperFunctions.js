8e167e2834294b53d2c4455b5e8266c14203eea1
Bryan Yunis <bbyunis@gmail.com>
2018-08-19 14:39:07 -0600

    initial branch commit

--- assets/javascript/helperFunctions.js
+++ assets/javascript/helperFunctions.js
@@ -2,6 +2,24 @@
 var pointsArr = response.routes[0].overview_path;
 
 //returns array of latLng objects
-var latLngs = response.routes[0].overview_path.map(function(point) {
+var latLngs = response.routes[0].overview_path.map(function (point) {
     return google.maps.LatLng(point.lat(), point.lng());
 });
+
+for (var i = 0; i < pointsArr.length; i++) {
+    if (i % 3 == 0) {
+        pointsArrDiv3.push(pointsArr[i]);
+    }
+}
+
+//get specified number of return values
+
+function getSpecificNumOfPts(num) {
+    var interval = Math.floor(pointsArr.length / 3);
+    for (var i = 0; i < pointsArr.length; i++) {
+        if (i % interval == 0) {
+            pointsArrDivArb.push(pointsArr[i]);
+        }
+    }
+
+}
